d23520ec8d4b1f61d81ef8d568435881adb38e98
Precyzik <gawaprecyz@gmail.com>
2025-10-20 19:26:52 +0200

    Reorganizing modules.

---
+++ set/enum-set/src/main/java/com/dosomedev/App.java
@@ -0,0 +1,81 @@
+package com.dosomedev;
+
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+public class App {
+    private static final int ROUNDS = 6;
+
+    private static final int ITERATIONS = 100000000;
+
+    private enum Color {
+        RED,
+        GREEN,
+        BLUE,
+        YELLOW,
+        ORANGE,
+        PURPLE
+    }
+
+    private enum BenchmarkType {
+        ADD,
+        CONTAINS,
+        REMOVE
+    }
+
+    public static void main(String[] args) {
+        runBenchmarkType(BenchmarkType.ADD);
+        runBenchmarkType(BenchmarkType.CONTAINS);
+        runBenchmarkType(BenchmarkType.REMOVE);
+    }
+
+    private static void runBenchmarkType(BenchmarkType type) {
+        Color[] colors = Color.values();
+        EnumSet<Color> enumSet = EnumSet.allOf(Color.class);
+        HashSet<Color> hashSet = new HashSet<>(Arrays.asList(colors));
+
+        long avgEnumSet = 0;
+        long avgHashSet = 0;
+
+        // Benchmark loop.
+        for (int i = 0; i < ROUNDS; i++) {
+            avgEnumSet += benchmark(enumSet, i + 1, type);
+            avgHashSet += benchmark(hashSet, i + 1, type);
+        }
+
+        // Calculate average millis.
+        avgEnumSet /= ROUNDS;
+        avgHashSet /= ROUNDS;
+
+        IO.println("Average EnumSet-" + type.toString() + " millis: " + avgEnumSet + " ms");
+        IO.println("Average HashSet-" + type.toString() + " millis: " + avgHashSet + " ms");
+    }
+
+    private static long benchmark(Set<Color> set, int round, BenchmarkType type) {
+        Color[] colors = Color.values();
+
+        long start = System.nanoTime();
+
+        if (type == BenchmarkType.ADD) {
+            for (int i = 0; i < ITERATIONS; i++) {
+                set.add(colors[i % colors.length]);
+            }
+        } else if (type == BenchmarkType.CONTAINS) {
+            for (int i = 0; i < ITERATIONS; i++) {
+                set.contains(colors[i % colors.length]);
+            }
+        } else if (type == BenchmarkType.REMOVE) {
+            for (int i = 0; i < ITERATIONS; i++) {
+                set.remove(colors[i % colors.length]);
+            }
+        }
+
+        long end = System.nanoTime();
+        // Calculate elapsed time.
+        long elapsedMillis = TimeUnit.NANOSECONDS.toMillis(end - start);
+
+        //IO.println("Round " + round + ": " + set.getClass().getSimpleName() + " elapsed time: " + elapsedMillis + " ms");
+
+        return elapsedMillis;
+    }
+}
---
+++ set/tree-set/src/main/java/com/dosomedev/App.java
@@ -0,0 +1,38 @@
+package com.dosomedev;
+
+import java.util.TreeSet;
+
+public class App {
+    static void main(String[] args) {
+
+        TreeSet<String> names = new TreeSet<>();
+        names.add("Olivia");
+        names.add("Noah");
+        names.add("Charlotte");
+        names.add("Olivia");
+
+        if (names.contains("Riley")) {
+            IO.println("Riley is in the set.");
+        } else {
+            IO.println("Riley is not in the set.");
+        }
+
+        IO.println("Total names: " + names.size());
+
+        names.remove("Noah");
+        IO.println("Removed Noah.");
+
+        IO.println("List of names:");
+        for (String name : names) {
+            IO.println("- " + name);
+        }
+
+        names.clear();
+        IO.println("Emptied the set.");
+
+        IO.println("List of names:");
+        for (String name : names) {
+            IO.println("- " + name);
+        }
+    }
+}
