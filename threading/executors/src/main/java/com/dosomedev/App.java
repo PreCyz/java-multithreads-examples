ba82bcfeeed620518a95096a757eeb46b612055b
Precyzik <gawaprecyz@gmail.com>
2025-10-20 19:51:34 +0200

    Preparing executor example.

--- threading/executors/src/main/java/com/dosomedev/App.java
+++ threading/executors/src/main/java/com/dosomedev/App.java
@@ -1,32 +1,77 @@
 package com.dosomedev;
 
 import java.math.*;
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
 
 public class App {
     final static int PRECISION = 100;
-
     final static int TERMS = 4000;
-
     final static int POOL = 2;
+//    final static int POOL = Runtime.getRuntime().availableProcessors();
+    final static int TASKS = 10;
 
-    final static int THREADS = 10;
-
-
-    public static void main(String[] args) {
+    static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
         System.out.printf("Precision: %s%n", PRECISION);
         System.out.printf("Terms:     %s%n", TERMS);
         System.out.printf("Pool:      %s%n", POOL);
-        System.out.printf("Threads:   %s%n", THREADS);
+        System.out.printf("Tasks:     %s%n", TASKS);
 
-        // Define executor pool.
-        //ExecutorService executor = Executors.newFixedThreadPool(POOL);
-        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(POOL);
+        try (ExecutorService executor = Executors.newFixedThreadPool(POOL)) {
 
-        // Define process.
-        Callable<BigDecimal> callable = new Callable<>() {
+            Callable<BigDecimal> callable = calculateEulerNumber();
+
+            List<Future<BigDecimal>> tasks = new ArrayList<>();
+            for (int i = 1; i <= TASKS; i++) {
+                Future<BigDecimal> task = executor.submit(callable);
+                tasks.add(task);
+            }
+
+            executor.shutdown();
+
+            processTasksWithThreadPoolExecutor((ThreadPoolExecutor) executor, tasks);
+
+        } finally {
+            System.out.printf("%nDone in %d sec.%n", Duration.between(now, LocalDateTime.now()).toSeconds());
+        }
+    }
+
+    private static void processTasksWithThreadPoolExecutor(ThreadPoolExecutor executor, List<Future<BigDecimal>> tasks) {
+        boolean executorTerminated = false;
+        while (!executorTerminated) {
+            if (executor.isTerminated()) {
+                executorTerminated = true;
+            }
+
+            boolean shutdown = executor.isShutdown();
+            boolean terminated = executor.isTerminated();
+            int pending = executor.getQueue().size();
+            int active = executor.getActiveCount();
+            long completed = executor.getCompletedTaskCount();
+            System.out.printf("pending: %s, active: %s, completed: %s, shutdown: %s, terminated: %s%n",
+                    pending, active, completed, shutdown, terminated);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.err.println("Sleep interrupted!");
+            }
+        }
+
+        try {
+            BigDecimal eulersNumber = tasks.getFirst().get();
+            System.out.printf("First thread result: %s%n", eulersNumber);
+        } catch (InterruptedException | ExecutionException ex) {
+            System.err.println("Could not grab result!");
+        }
+    }
+
+    private static Callable<BigDecimal> calculateEulerNumber() {
+        return new Callable<>() {
             @Override
             public BigDecimal call() throws Exception {
                 MathContext mc = new MathContext(PRECISION, RoundingMode.HALF_UP);
@@ -49,40 +94,5 @@ public class App {
                 return result;
             }
         };
-
-        List<Future<BigDecimal>> tasks = new ArrayList<>();
-        for (int i = 1; i <= THREADS; i++) {
-            Future<BigDecimal> task = executor.submit(callable);
-            tasks.add(task);
-        }
-
-        executor.shutdown();
-
-        boolean executorTerminated = false;
-        while (!executorTerminated) {
-            if (executor.isTerminated()) {
-                executorTerminated = true;
-            }
-
-            boolean shutdown = executor.isShutdown();
-            boolean terminated = executor.isTerminated();
-            int pending = executor.getQueue().size();
-            int active = executor.getActiveCount();
-            long completed = executor.getCompletedTaskCount();
-            System.out.printf("pending: %s, active: %s, completed: %s, shutdown: %s, terminated: %s%n", pending, active, completed, shutdown, terminated);
-
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                System.err.println("Sleep interrupted!");
-            }
-        }
-
-        try {
-            BigDecimal eulersNumber = tasks.get(0).get();
-            System.out.printf("First thread result: %s%n", eulersNumber);
-        } catch (InterruptedException | ExecutionException ex) {
-            System.err.println("Could not grab result!");
-        }
     }
 }
