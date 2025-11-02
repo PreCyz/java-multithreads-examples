5011af3ed2025789f89531a9fea448049eb53d9c
Precyzik <gawaprecyz@gmail.com>
2025-11-02 11:23:33 +0100

    Cleaning.

--- threading/fork-join-pool/src/main/java/com/dosomedev/App.java
+++ threading/fork-join-pool/src/main/java/com/dosomedev/App.java
@@ -4,16 +4,17 @@ import com.dosomedev.sort.SortExample;
 
 public class App {
     static void main(String[] args) {
+        IO.println("\n==================================================\n");
         new MatrixMultiplicationExample().run();
         new MatrixMultiplicationForkJoinExample().run();
-        IO.println("\n==================================================\n");
 
+        IO.println("\n==================================================\n");
         int nrows = 200;
         int ncols = 200;
         new MatrixMultiplicationBigExample(nrows, ncols).run();
         new MatrixMultiplicationForkJoinBigExample(ncols, nrows).run();
-        IO.println("\n==================================================\n");
 
+        IO.println("\n==================================================\n");
         new SortExample(400_000_000).run();
     }
 }
--- threading/fork-join-pool/src/main/java/com/dosomedev/sort/SortExample.java
+++ threading/fork-join-pool/src/main/java/com/dosomedev/sort/SortExample.java
@@ -1,11 +1,11 @@
 package com.dosomedev.sort;
 
+import lombok.RequiredArgsConstructor;
+
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.Random;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.ForkJoinTask;
-import lombok.RequiredArgsConstructor;
+import java.util.concurrent.*;
 
 @RequiredArgsConstructor
 public class SortExample implements Runnable {
@@ -13,33 +13,39 @@ public class SortExample implements Runnable {
 
     @Override
     public void run() {
+        LocalDateTime start = LocalDateTime.now();
         var random = new Random();
         int[] data = new int[tableSize];
         for (int i = 0; i < tableSize; i++) {
             data[i] = random.nextInt(tableSize);
         }
+        Duration duration = Duration.between(start, LocalDateTime.now());
+        System.out.printf("Random table created in %d.%dsec%n", duration.toSecondsPart(), duration.toMillisPart());
         System.out.printf("Table: first [%d], last [%d]%n", data[0], data[data.length - 1]);
 
-        LocalDateTime start = LocalDateTime.now();
         try (ForkJoinPool pool = ForkJoinPool.commonPool()) {
             System.out.printf("Pool parallelism: %d%n", pool.getParallelism());
 
             QuickSortAction task = new QuickSortAction(data, 0, tableSize - 1);
-            // `invoke` blocks program
+            start = LocalDateTime.now();
+            //`invoke` blocks program
             pool.invoke(task);
 
-            //submits does not block program
-            //submit(pool, task);
+            //`submit` doesn't block program
+//            submit(pool, task);
 
-            //async execution
+            //'execute' doesn't block program - async execution
 //            execute(pool, task);
 
-            pool.shutdown();
+            //according to the specification: if you use commonPool() then Any program that relies on asynchronous task
+            //processing to complete before program termination should invoke commonPool().awaitQuiescence, before exit
+            pool.awaitQuiescence(10, TimeUnit.SECONDS);
+
         } catch (Exception e) {
             System.err.println(e.getMessage());
         } finally {
             System.out.printf("Table sorted: first [%d], last [%d]%n", data[0], data[data.length - 1]);
-            Duration duration = Duration.between(start, LocalDateTime.now());
+            duration = Duration.between(start, LocalDateTime.now());
             System.out.printf("ForkJoinPool completed in %d.%dsec%n", duration.toSecondsPart(), duration.toMillisPart());
         }
 
@@ -51,20 +57,21 @@ public class SortExample implements Runnable {
         submit.join();
     }
 
-    private void execute(ForkJoinPool pool, ForkJoinTask<Void> task) throws InterruptedException {
-        pool.execute(task);
-        printPoolDetails("execute", pool, task);
-        task.join();
-    }
-
     private void printPoolDetails(String operation, ForkJoinPool pool, ForkJoinTask<Void> task) throws InterruptedException {
         System.out.printf("%s QueuedSubmissionCount: %d%n", operation, pool.getQueuedSubmissionCount());
 
+        //this blocks the program
         while (!task.isDone()) {
             System.out.printf("ActiveThreadCount: %d, ", pool.getActiveThreadCount());
             System.out.printf("QueuedTaskCount: %d, ", pool.getQueuedTaskCount());
             System.out.printf("StealCount: %d%n", pool.getStealCount());
             Thread.sleep(Duration.ofMillis(100));
         }
+    }
+
+    private void execute(ForkJoinPool pool, ForkJoinTask<Void> task) throws InterruptedException {
+        pool.execute(task);
+        printPoolDetails("execute", pool, task);
+        task.join();
     }
 }
